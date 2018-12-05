34b1d756f169975f8327d9500229a444bb6d08c9
Ryan Achten <ryanachten@gmail.com>
2018-12-05 19:23:52 +1300

    Add init input elements to LoginScreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   Image,
+  TextInput,
   Platform,
   ScrollView,
   StyleSheet,
@@ -15,7 +16,28 @@ export default class LoginScreen extends React.Component {
     return (
       <View>
         <Text>Login Screen yo!</Text>
+        <TextInput style={styles.input} placeholderTextColor='rgb(64, 64, 64)' placeholder='email@test.com' />
+        <TextInput style={styles.input} placeholderTextColor='rgb(64, 64, 64)' placeholder='password' />
+        <TouchableOpacity style={styles.button}>
+          <Text>Here's a button yo</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: 'rgb(200, 200, 200)',
+    padding: 20,
+    margin: 20
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgb(200, 200, 200)',
+    padding: 20,
+    flex: 1,
+    margin: 20
+  }
+});
