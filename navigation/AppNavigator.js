08b6f48020850fda7e9c82f7752ff060dd2005c5
Ryan Achten <ryanachten@gmail.com>
2018-12-05 18:57:52 +1300

    Add auth navigation flow, Add init login screen template

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
+import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 
+import LoginScreen from '../screens/LoginScreen';
 import MainTabNavigator from './MainTabNavigator';
+
+const AuthStack = createStackNavigator({ SignIn: LoginScreen });
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
+  Auth: AuthStack,
+
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
 });
---
+++ screens/LoginScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default class LoginScreen extends React.Component {
+
+  render() {
+    return (
+      <View>
+        <Text>Login Screen yo!</Text>
+      </View>
+    );
+  }
+}
